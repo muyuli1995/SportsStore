76d1700eca04810956dce4656d81a668c89f1a3f
MuyuLi <muyu-li@hotmail.com>
2019-09-08 09:46:23 -0400

    1[CHG] change EFProductRepository

--- vic.SportStore/Vic.SportsStore.Domain/Concrete/EFProductRepository.cs
+++ vic.SportStore/Vic.SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Entities;
 
 namespace Vic.SportsStore.Domain.Concrete
 {
-    public class EFProductRepository : IProductsRepository
+    public class EFProductRepository: IProductsRepository
     {
         private EFDbContext context = new EFDbContext();
         public IEnumerable<Product> Products
         {
             get { return context.Products; }
         }
+
+      
     }
 }
